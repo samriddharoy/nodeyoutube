5f4d20da13ed65ae84b7f0dbbffe2b0829ffa137
samriddha roy chowdhury <roychowdhurysamriddha@gmail.com>
2025-02-26 23:44:01 +0530

    server error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import dotenv from "dotenv";
 import conn from "./db/connection.js";
+import express from "express";
+
+const app = express();
 
 // Load environment variables
 dotenv.config();
 
 // Establish Database Connection
 conn()
-  
+.then(() => {
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server running on port ${process.env.PORT}`);  // Log the server's port number
+    } );
+})
+.catch((error) => {
+    console.error(" Error connecting to MongoDB:", error);
+    process.exit(1); // Exit on failure
+})
